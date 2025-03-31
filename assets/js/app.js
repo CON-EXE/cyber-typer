115b812a01b741e312e6d0ed1844a8ec344928d6
Connor Hall <connrhall204@gmail.com>
2025-03-31 13:24:30 -0500

    Added modules and additional files

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,37 +7,9 @@ let word = '';
 let shuffledWords = [];
 let timer;
 
-const words = [
-    'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'weather',
-    'bottle', 'history', 'dream', 'character', 'money', 'absolute', 'machine',
-    'accurate', 'rainbow', 'bicycle', 'eclipse', 'trouble', 'developer',
-    'database', 'periodic', 'fortune', 'phone', 'future', 'pasta', 'microwave',
-    'jungle', 'wallet', 'canada', 'velvet', 'potion', 'treasure', 'beacon',
-    'whisper', 'breeze', 'coffee', 'beauty', 'agency', 'chocolate', 'eleven',
-    'alphabet', 'magician', 'triangle', 'baseball', 'beyond', 'banana', 'perfume',
-    'computer', 'butterfly', 'music', 'eagle', 'crown', 'chess', 'laptop',
-    'bedroom', 'enemy', 'button', 'door', 'bird', 'superman', 'library',
-    'bookstore', 'language', 'homework', 'beach', 'economy', 'awesome',
-    'science', 'mystery', 'famous', 'league', 'memory', 'leather', 'planet',
-    'software', 'update', 'yellow', 'keyboard', 'window', 'beans', 'truck',
-    'sheep', 'blossom', 'secret', 'wonder', 'destiny', 'quest', 'download',
-    'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'audio', 'school',
-    'detective', 'hero', 'progress', 'winter', 'passion', 'rebel', 'amber',
-    'jacket', 'article', 'paradox', 'social', 'resort', 'mask', 'escape',
-    'promise', 'band', 'level', 'hope', 'moonlight', 'media', 'orchestra',
-    'volcano', 'guitar', 'raindrop', 'diamond', 'illusion', 'firefly', 'ocean',
-    'cascade', 'journey', 'laughter', 'horizon', 'marvel', 'compiler', 'twilight',
-    'harmony', 'symphony', 'solitude', 'essence', 'forest', 'melody',
-    'vision', 'silence', 'eternity', 'embrace', 'poet', 'ricochet', 'mountain',
-    'dance', 'sunrise', 'dragon', 'adventure', 'galaxy', 'echo', 'fantasy',
-    'radiant', 'mermaid', 'legend', 'monitor', 'plastic', 'pressure', 'bread',
-    'cake', 'caramel', 'juice', 'mouse', 'charger', 'pillow', 'candle', 'sunset',
-    'farmer', 'garden', 'whistle', 'blanket', 'picnic', 'sweater', 'lantern',
-    'theater', 'traffic', 'website', 'courage', 'shelter', 'painter', 'twinkle',
-    'squeeze', 'forever', 'stadium', 'gourmet', 'flower', 'bravery', 'playful',
-    'captain', 'vibrant', 'damage', 'outlet', 'general', 'batman', 'enigma',
-    'storm', 'universe', 'engine', 'mistake', 'hurricane', 'jasper'
-];
+import wordList from "./words.js";
+import * as utils from "./utils.js";
+// import Score from "./Score.js";
 
 const bgm = new Audio('./assets/media/bgm.mp3');
 const scoreWord = new Audio('./assets/media/get-word.mp3');
@@ -110,7 +82,7 @@ startBtn.addEventListener('click', () => {
     startBtn.classList.add('hide');
     resetBtn.classList.remove('hide');
 
-    shuffledWords = shuffle(words);
+    shuffledWords = shuffle(wordList);
     getWord(shuffledWords);
 });
 
@@ -167,6 +139,6 @@ resetBtn.addEventListener('click', () => {
     playerInput.focus();
     startBtn.classList.add('hide');
     resetBtn.classList.remove('hide');
-    shuffledWords = shuffle(words);
+    shuffledWords = shuffle(wordList);
     getWord(shuffledWords);
 })
